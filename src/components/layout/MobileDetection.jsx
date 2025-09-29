ad6a9ec7ad214ce8192daaee3b085cd50d29eb7f
Shant761 <flashbaracademia@gmail.com>
2025-09-29 22:13:45 +0400

    Update MobileDetection.jsx

--- src/components/layout/MobileDetection.jsx
+++ src/components/layout/MobileDetection.jsx
@@ -1,1 +1,12 @@
-db
+import React, { useEffect } from 'react';
+
+export default function MobileDetection() {
+  useEffect(() => {
+    const handleTouchStart = () => {
+      document.documentElement.classList.add('touch-device');
+    };
+    document.addEventListener('touchstart', handleTouchStart, { once: true });
+    return () => document.removeEventListener('touchstart', handleTouchStart);
+  }, []);
+  return null;
+}
